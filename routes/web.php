1bdb72c0d9678e2a998e2f0790425a21ea0d1953
joao-avelino-92 <joaoavnt@gmail.com>
2024-06-05 00:48:18 -0300

    clean and organize routes

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@
         $router->get('user', ['as' => 'getUserbyId', 'uses' => 'UserController@getUsers']);
         $router->get('user/{id}', ['as' => 'getUserbyId', 'uses' => 'UserController@getUserById']);
         $router->get('user/info/account', ['as' => 'getUserByToken', 'uses' => 'UserController@getUserByToken']);
-        $router->post('user/transfer', ['as' => 'postTransfer', 'uses' => 'TransferController@postTransfer']);
     });
 
 //Wallet routes
@@ -31,19 +30,5 @@
         $router->get('wallet/{id}', ['as' => 'getWalletById', 'uses' => 'WalletController@getWalletById']);
     });
 
-//test Email
-    $router->get('/test_mail', function () {
-        $data = ['test data'];
-        try {
-            $arrayUser['name'] = 'teste';
-            $arrayUser['email'] = 'joaoavnt@gmail.com';
-            Mail::send('email.transfer.email', $arrayUser, function (Message $message) use ($arrayUser) {
-                $message->to($arrayUser['email'], $arrayUser['name'])
-                        ->from('joaoavnt@gmail.com', 'digital-bank-api')
-                        ->subject('Test Mail');
-            });
-        } catch (\Exception $exception) {
-            throw new Exception($exception);
-        }
-//        dd('passou');
-    });
+//transfer
+    $router->post('transfer', ['as' => 'postTransfer', 'uses' => 'TransferController@postTransfer']);
